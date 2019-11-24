9afd6a6e897449e4892a5af6aed301b5588025eb
dorota <dorota.zelga@gmail.com>
2019-11-24 13:25:17 +0100

    learning routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const PageOne = () => {
+    return <div>Page One</div>
+};
+
+const PageTwo = () => {
+    return <div>Page Two</div>
+};
 
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <BrowserRouter>
+                <Route path='/' exact component={PageOne}/>
+                <Route path='/pagetwo' compoenent={PageTwo}/>
+            </BrowserRouter>
+        </div>
     )
 };
 
