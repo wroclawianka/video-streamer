18554d22e75cb35a7fb96938c15a954ff5c5217f
Dorota Zelga <dorota.zelga@gmail.com>
2019-12-07 19:53:58 +0100

    header component added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
@@ -10,6 +11,7 @@ const App = () => {
     return (
         <div>
             <BrowserRouter>
+                <Header/>
                 <Route path='/' exact component={StreamList}/>
                 <Route path='/streams/new' exact component={StreamCreate}/>
                 <Route path='/streams/edit' exact component={StreamEdit}/>
