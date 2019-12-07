8648641eb269dbb0587097170d4efd161c2bd7c6
Dorota Zelga <dorota.zelga@gmail.com>
2019-12-07 23:23:20 +0100

    redux devtools configuration

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {applyMiddleware, compose, createStore} from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
 import './index.css';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware())
+);
 
 ReactDOM.render(
     <Provider store={store}>
