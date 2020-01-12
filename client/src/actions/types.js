ef9f0e453c03815b1ebed86cc2cbff7aabf3d5a4
Dorota Zelga <dorota.zelga@gmail.com>
2020-01-12 15:09:30 +0100

    fetch single stream, fetch list, edit and delete actions created

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,7 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
+export const CREATE_STREAM = 'CREATE_STREAM';
+export const FETCH_STREAM_LIST = 'FETCH_STREAM_LIST';
+export const FETCH_STREAM = 'FETCH_STREAM';
+export const DELETE_STREAM = 'DELETE_STREAM';
+export const EDIT_STREAM = 'EDIT_STREAM';
