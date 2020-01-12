b07f039d2e6a14ecec188eab770fbc4d1f89eeb5
Dorota Zelga <dorota.zelga@gmail.com>
2020-01-12 14:23:06 +0100

    code formatting

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { Field, reduxForm } from 'redux-form';
-import { connect } from 'react-redux';
-import { createStream } from '../../actions'
+import {Field, reduxForm} from 'redux-form';
+import {connect} from 'react-redux';
+import {createStream} from '../../actions'
 
 class StreamCreate extends React.Component {
-    renderError({ error, touched }) {
-        if(touched && error) {
+    renderError({error, touched}) {
+        if (touched && error) {
             return (
                 <div className="ui error message">
                     <div className="header">{error}</div>
                 </div>
-                
+
             )
         }
     }
 
 
-    renderInput = ({ input, label, meta }) => {
-        console.log(meta.error)
-        console.log(meta.touched)
+    renderInput = ({input, label, meta}) => {
         const className = `field ${meta.error && meta.touched ? 'error' : ''}`
         return (
             <div className={className}>
@@ -27,7 +25,7 @@ class StreamCreate extends React.Component {
                 {this.renderError(meta)}
             </div>
         )
-    }
+    };
 
     onSubmit(formValues) {
         //event.preventDefault()
@@ -36,7 +34,7 @@ class StreamCreate extends React.Component {
 
     render() {
         return (
-            <form 
+            <form
                 onSubmit={this.props.handleSubmit(this.onSubmit)}
                 className="ui form error"
             >
@@ -52,11 +50,11 @@ class StreamCreate extends React.Component {
 const validate = (formValues) => {
     const errors = {};
 
-    if( !formValues.title) {
+    if (!formValues.title) {
         errors.title = "You must enter a title"
     }
 
-    if( !formValues.description) {
+    if (!formValues.description) {
         errors.description = "You must enter a description"
     }
 
