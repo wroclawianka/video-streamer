d78cba262e2195b64309d2aa9d1581be2234309a
Dorota Zelga <dorota.zelga@gmail.com>
2020-01-25 14:57:00 +0100

    StreamList to class component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -2,10 +2,14 @@ import React from "react";
 import {connect} from 'react-redux';
 import {fetchStreamList} from '../../actions'
 
-const StreamList = () => {
-    return (
+class StreamList extends React.Component {
+    componentDidMount() {
+        this.props.fetchStreamList();
+    }
+
+    render() {
         <div>StreamList</div>
-    )
-};
+    }
+}
 
 export default connect(null, {fetchStreamList})(StreamList);
