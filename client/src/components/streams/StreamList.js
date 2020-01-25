e8d831d753e212b0022fc59fcd73fa0cb796f7db
Dorota Zelga <dorota.zelga@gmail.com>
2020-01-25 14:54:03 +0100

    connect StreamList component with action

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {connect} from 'react-redux';
+import {fetchStreamList} from '../../actions'
 
 const StreamList = () => {
     return (
@@ -6,4 +8,4 @@ const StreamList = () => {
     )
 };
 
-export default StreamList;
+export default connect(null, {fetchStreamList})(StreamList);
