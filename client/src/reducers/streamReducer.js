88eb7eee369c09da4a45ede06f49088667731382
Dorota Zelga <dorota.zelga@gmail.com>
2020-01-25 13:02:30 +0100

    streamReducer.js created

---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,14 @@
+import {
+    FETCH_STREAM,
+    FETCH_STREAM_LIST,
+    CREATE_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from '../actions/types'
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
